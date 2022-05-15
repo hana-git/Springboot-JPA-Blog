495c4e6bea00d7e8a8aa0f303eb3c8752e0d34ca
hana-git <hana911223@gmail.com>
2022-05-15 10:48:42 +0900

    spring security 회원가입

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.cos.blog.config;
 
+import org.aspectj.weaver.bcel.BcelAccessForInlineMunger;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 //빈등록의미 : spring 컨테이너에서 객체를 관리할 수 있게 하는것
 @Configuration //빈등록(IoC관리)
@@ -12,11 +15,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(prePostEnabled = true) //특정 주소로 접근하면 권한 및 인증을 미리 체크하겠다는 의미
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean //IoC (BCryptPasswordEncoder 를 스프링이 관리한다)
+    public BCryptPasswordEncoder encodePWD(){
+        //String encPassword = new BCryptPasswordEncoder().encode("1234");//이 객체를 통해서 암호화할수있다
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable() //csrf토큰 비활성화
+                //요청시에 csrf 토큰이 없을경우 spring security 막아버리는게 기본 / 따라서 테스트할때는 disable하는게 좋음
                 .authorizeRequests() //요청이 들어올때
-                    .antMatchers("/auth/**") //auth 하위로 들어오는건
+                    .antMatchers("/","/auth/**", "/js/**", "/css/**", "/image/**") //auth 하위로 들어오는건
                     .permitAll()  // 모두 허용한다.
                     .anyRequest() //이게아닌 다른 모든 요청은
                     .authenticated()  //인증이 필요하다
--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -1,8 +1,10 @@
 package com.cos.blog.service;
 
+import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,9 +15,17 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
     @Transactional //여러개의 트랜젝션이 성공하면 commit, 한개라도 실패하면 rollback
     public int 회원가입(User user){
         try{
+            String rawPassword = user.getPassword(); //1234 원문pw
+            String encPassword = encoder.encode(rawPassword); //해쉬pw
+            user.setPassword(encPassword);
+            //실제로 DB에 insert하고 아래에서 return되면 됨.
+            user.setRole(RoleType.USER);
             userRepository.save(user);
             return 1;
         }catch (Exception e){
