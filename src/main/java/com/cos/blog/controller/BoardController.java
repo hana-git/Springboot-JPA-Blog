166bcafe83a1297d5e3ddd013e1c9596fc98fd89
hana-git <hana911223@gmail.com>
2022-05-15 13:08:09 +0900

    spring security 컨트롤러 세션 확인

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,13 +1,16 @@
 package com.cos.blog.controller;
 
+import com.cos.blog.auth.PrincipalDetail;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
 
-    @GetMapping({"","/"})
-    public String index(){
+    @GetMapping({"","/"}) //컨트롤러에서 세션을 어떻게 찾는지?
+    public String index(@AuthenticationPrincipal PrincipalDetail principal){
+        System.out.println("로그인 사용자 아이디 : " + principal.getUsername());
         return "index";
     }
 }
